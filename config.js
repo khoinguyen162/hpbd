656306aac8a6f970690bb7b82f94feba434cb5ea
khoinguyen162 <kn4267909@gmail.com>
2025-12-07 14:35:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ export const PASSWORD = "13122007"; //sửa pass ở đây
 
 //sửa phần text vs btn
 export const noData = [
-    { text: "Anh chắc chưa đó?", btn: "Thật sao anh", img: "./assets/images/2.gif" },
-    { text: "Cục cưng ơi", btn: "Anh chắc chưa đó?", img: "./assets/images/3.gif" },
-    { text: "Đừng làm vậy với em", btn: "Đừng làm vậy với Em", img: "./assets/images/4.gif" },
-    { text: "Tim em đang tan vỡ", btn: "Tim em đang tan vỡ", img: "./assets/images/5.gif" },
-    { text: "Em sắp khóc rồi đó", btn: "Em sắp khóc rồi đó", img: "./assets/images/6.gif" }
+    { text: "Sao Lại Hông Daa", btn: "Hông", img: "./assets/images/2.gif" },
+    { text: "Dạ Đi Mòoo", btn: "Hông", img: "./assets/images/3.gif" },
+    { text: "Sao Lì Theee", btn: "Hông", img: "./assets/images/4.gif" },
+    { text: "Tui Bất Lực Roài Nhee", btn: "Hông", img: "./assets/images/5.gif" },
+    { text: "Để Tui Khóc Cho Mấy Người Dừa Lòng", btn: "Em sắp khóc rồi đó", img: "./assets/images/6.gif" }
 ];
 
 //sửa phần thư
