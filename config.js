6fa094fb1f7c282831db786ec5cbec0f27f28a50
khoinguyen162 <kn4267909@gmail.com>
2025-12-07 14:40:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,11 @@ export const noData = [
 ];
 
 //sửa phần thư
-export const letterHeading = "Gửi em,";
+export const letterHeading = "Happy Birth Day Vợ Iu,";
 
-export const letterText = `Cảm ơn em đã bước đến một cách mãnh liệt nhưng cũng dịu dàng đến thế.`;
+export const letterText = `Tadaa, hôm nay là ngày siu siu đặc biệt lunn nhee, hôm nay là ngày mà 1 em bé được ra đời, em bé đó có tên là Nguyễn Hoàng Yến Nhi là vợ iu của anh. Từ lúc em bé đến với anh, em bé dạy với mang đến cho anh nhiều điều lắm. Em dạy cho anh biết cách nhẹ nhàng, biết cách dịu dàng, biết cách nhẫn nhịn,quan tâm, lo lắng, biết cách iu thương, biết cách tự tế, mặc dù hong phải là tình đầu nhưng lần đầu anh có cảm giác mình được iu như thế, cho anh biết được là sau lưng vẫn còn 1 người quan tâm và lo lắng cho anh, cho anh biết được là dù sau này mình có ra sao đi nữa thì đối với anh thì em vẫn sẽ là người mang lại cho anh nhiều cảm xúc mà từ trước giờ có lẽ anh chưa được trải qua bao giờ. Anh cảm thấy iu em bé nhiều lắm❤❤. Anh chúc em bé của anh tuổi mới mọi điều em làm sẽ được suông sẻ, mọi may mắn và những điều tốt đẹp nhất sẽ đến với em vì em xứng đáng có được những điều đó, đặc biệt là mọi điều hong vui mọi sự tiêu cực ở hiện tại và quá khứ của em sẽ được anh đem giấu mất hết hehe. Chúc em bé sẽ có được thật thật nhiều sức khỏe để có thể làm việc mà hong mệt lunnn, và hy vọng anh sẽ ở bên cạnh em lâu hơn hơn hơn nữa để những lúc em mệt mỏi thì còn có anh làm chỗ dựa cho em những lúc em mệt nhé. Anh iu vợ nhiều nhiều lắmmmm ❤❤.
+Nói chung là chúc em có tất cả những điều măn mắn và mất đi những điều hong tốt hee  
+
+                                                    Ký tên
+
+                                              Chongiu của em ❤❤`;
